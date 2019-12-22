e2bdae777814d5ad00f7e4f131fe45f8f3507451
Jon Scheiding <jonscheiding@gmail.com>
2019-12-22 12:56:08 -0500

    Add text content on win page

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ class App extends React.Component {
   render() {
     this.videoRef = React.createRef();
 
+    const content = process.env.REACT_APP_TEXT_CONTENT.split('|');
+    const [firstLine, ...remainingLines] = content;
+
     return (
       <div className='app'>
         <div className={cx({'lock-overlay': true, 'opened': this.state.opened})}>
@@ -30,6 +33,8 @@ class App extends React.Component {
             videoId={process.env.REACT_APP_VIDEO_ID} 
             opts={{width: '100%', playerVars: { playsinline: 1 }}}
             onReady={e => this.videoTarget = e.target} />
+          <h1>{firstLine}</h1>
+          {remainingLines.map(l => <h2>{l}</h2>)}
         </div>
       </div>
     );
